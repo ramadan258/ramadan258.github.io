27c15a5b98e889577459a4cb9d78f486bbd65316
ramadan258 <179522853+ramadan258@users.noreply.github.com>
2026-09-14 20:37:38 +0200

    Set steadfastness deadline to end of Jumada al-Awwal

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 ﻿    const CONFIG = {
-      AHD_TARGET_DATE_ISO: "2026-10-11T23:59:59+03:00",
+      AHD_TARGET_DATE_ISO: "2026-11-10T23:59:59+01:00",
       FORCE_HIZB_NUMBER: null,
 
       LINKS: {
